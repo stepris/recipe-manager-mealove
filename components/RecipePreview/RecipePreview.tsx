32ed22ce80cf13e777e16a9cc4a9810de0f38f12
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-07-25 14:33:45 +0200

    add basic RecipePreview component

---
+++ components/RecipePreview/RecipePreview.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function RecipePreview() {
+  return <StyledRecipePreview></StyledRecipePreview>;
+}
+
+const StyledRecipePreview = styled.article`
+  background-color: grey;
+  width: 100px;
+  height: 100px;
+`;
