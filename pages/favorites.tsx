37e4be66e4ece57bf1491ee7386a8aeabed009f6
gunnarmon <dev.gun.mon@gmail.com>
2024-08-02 18:25:18 +0200

    add graphical toggle function to favorites page

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,13 +1,18 @@
 import QuickLinks from '@/components/QuickLinks';
 import RecipeList from '@/components/RecipeList';
 
-export default function Favorites({ favoriteRecipes, onToggleFavorite }) {
+export default function Favorites({
+  favoriteRecipes,
+  onToggleFavorite,
+  favoriteRecipesList,
+}) {
   return (
     <>
       <QuickLinks $isFavorites />
       <RecipeList
         recipes={favoriteRecipes}
         onToggleFavorite={onToggleFavorite}
+        favoriteRecipesList={favoriteRecipesList}
       />
     </>
   );
