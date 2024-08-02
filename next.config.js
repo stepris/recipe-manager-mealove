84c7cb25bc9d47056272708f155c131cc2e86d5c
gunnarmon <dev.gun.mon@gmail.com>
2024-08-02 14:23:18 +0200

    change next.config.js to ignore typescript build errors for vercel

--- next.config.js
+++ next.config.js
@@ -22,6 +22,13 @@ const nextConfig = {
       },
     ],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
