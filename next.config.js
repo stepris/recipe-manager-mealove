716ce52e1eb81b601a2250ecbf100832ae70885a
gunnarmon <dev.gun.mon@gmail.com>
2024-08-05 14:35:01 +0200

    remove typescript ignoreBuildError from next.config

--- next.config.js
+++ next.config.js
@@ -22,13 +22,5 @@ const nextConfig = {
       },
     ],
   },
-  /*   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  }, */
 };
-
 module.exports = nextConfig;
