36e36c43c6c1dbca3030888f0f2b14dbb36e20ee
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-06 16:05:25 +0200

    use Router for navigation toggle

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -1,13 +1,24 @@
 import FilledNavigation from '@/public/icons/ic_fluent_navigation_24_filled.svg';
 import styled from 'styled-components';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function NavigationMenu() {
-  return (
-    <Link href={'/navigation'}>
-      <StyledFilledNavigation />
-    </Link>
-  );
+  const router = useRouter();
+  const page = router.pathname;
+
+  if (page === '/navigation') {
+    return (
+      <button type='button' onClick={() => router.back()}>
+        <StyledFilledNavigation />
+      </button>
+    );
+  } else {
+    return (
+      <button type='button' onClick={() => router.push('/navigation')}>
+        <StyledFilledNavigation />
+      </button>
+    );
+  }
 }
 
 const StyledFilledNavigation = styled(FilledNavigation)`
