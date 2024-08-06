c522c482a0442eee5bd2cc9839a0e46c13fdabf6
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-06 14:42:02 +0200

    add Link component to navigation

--- components/NavigationList.tsx
+++ components/NavigationList.tsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
+import Link from 'next/link';
 
 export default function NavigationList() {
   return (
-    <StyledList>
-      <StyledListElement>Explore</StyledListElement>
-      <StyledListElement>Favorites</StyledListElement>
-    </StyledList>
+    <nav>
+      <StyledList>
+        <StyledListElement>
+          <StyledLink href={'/'}>Explore</StyledLink>
+        </StyledListElement>
+        <StyledListElement>
+          <StyledLink href={'/favorites'}>Favorites</StyledLink>
+        </StyledListElement>
+      </StyledList>
+    </nav>
   );
 }
 
@@ -15,14 +22,18 @@ const StyledList = styled.ul`
   gap: var(--spacing-1);
   height: calc(100vh - 55px);
 `;
-
 const StyledListElement = styled.li`
+  flex-grow: 1;
+  background-color: var(--color-primary-1);
   text-transform: uppercase;
   font: var(--font-nav);
-  background-color: var(--color-primary-1);
   color: var(--color-neutral-1);
-  flex-grow: 1;
-  align-content: center;
   text-align: center;
   letter-spacing: var(--letter-spacing-s);
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+  height: 100%;
+  align-content: center;
 `;
