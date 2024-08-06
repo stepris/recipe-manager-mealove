db5a48b05b8939ad6b4f8711a71c7117ad3e3134
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-06 11:55:17 +0200

    add basic Navigation and NavigationMenu Component

---
+++ components/Navigation.tsx
@@ -0,0 +1,10 @@
+import NavigationMenu from './NavigationMenu';
+
+export default function Navigation() {
+  return (
+    <>
+      <NavigationMenu />
+      <h2>meaLove</h2>;
+    </>
+  );
+}
---
+++ components/NavigationMenu.tsx
@@ -0,0 +1,5 @@
+import FilledNavigation from '@/public/icons/ic_fluent_navigation_24_filled.svg';
+
+export default function NavigationMenu() {
+  return <FilledNavigation />;
+}
