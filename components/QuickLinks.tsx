1ad324e5ad2ccf6ff9e378f2568a541dbaa5ec76
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-07 17:02:19 +0200

    remove css import from sc in QuickLinks Component

--- components/QuickLinks.tsx
+++ components/QuickLinks.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { LinkProps } from '@/types';
 import { useRouter } from 'next/router';
 
