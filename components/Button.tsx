26528c0b19755e385d4c21bbc7fd5c0ee03053be
Gunnar Montag <dev.gun.mon@gmail.com>
2024-08-14 17:29:34 +0200

    add button icons to new folder in public. write switch for icon rendering.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,17 @@
 import styled, { css } from 'styled-components';
+import SaveIcon from '@/public/icons/buttons/ic_fluent_save_24_filled.svg';
+import EditIcon from '@/public/icons/buttons/ic_fluent_edit_24_filled.svg';
+import DismissIconSquare from '@/public/icons/buttons/ic_fluent_dismiss_square_24_filled.svg';
+import DismissIconCircle from '@/public/icons/buttons/ic_fluent_dismiss_circle_24_filled.svg';
+import CheckIcon from '@/public/icons/buttons/ic_fluent_checkmark_circle_24_filled.svg';
 
-export default function Button({ children, variant }) {
-  return <StyledButton variant={variant}>{children}</StyledButton>;
+export default function Button({ children, variant, type }) {
+  return (
+    <StyledButton variant={variant}>
+      {buttonType(type)}
+      {children}
+    </StyledButton>
+  );
 }
 
 const buttonVariants = (variant) => {
@@ -44,9 +54,31 @@ const buttonVariants = (variant) => {
   }
 };
 
+const buttonType = (type) => {
+  switch (type) {
+    case '$submit':
+      return <SaveIcon />;
+    case '$edit':
+      return <SaveIcon />;
+    case '$update':
+      return <EditIcon />;
+    case '$cancel':
+      return <DismissIconSquare />;
+    case '$delete':
+      return;
+    case '$yes':
+      return <CheckIcon />;
+    case '$no':
+      return <DismissIconCircle />;
+    default:
+      return null;
+  }
+};
+
 const StyledButton = styled.button`
   font: var(--font-caption);
   text-transform: uppercase;
+  display: flex;
   border-radius: var(--radius-m);
   padding: 0.3rem;
 
