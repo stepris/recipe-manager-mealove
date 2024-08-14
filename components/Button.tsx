07a437b71b96c89a7c5741ae01d5475fd02bf55c
Gunnar Montag <dev.gun.mon@gmail.com>
2024-08-14 20:56:23 +0200

    add delete icon. add hover for buttons and basic styling.

--- components/Button.tsx
+++ components/Button.tsx
@@ -42,7 +42,14 @@ export default function Button({ children, variant, type, onClickBehavior }) {
           />
         );
       case '$delete':
-        return;
+        return (
+          <Image
+            src='/icons/buttons/ic_fluent_delete_24_filled.svg'
+            width={20}
+            height={20}
+            alt='cancel'
+          />
+        );
       case '$yes':
         return (
           <Image
@@ -77,25 +84,63 @@ export default function Button({ children, variant, type, onClickBehavior }) {
   );
 }
 
+/* const buttonVariants = (variant) => {
+  switch (variant) {
+    case '$submit':
+      return css`
+        color: var(--color-neutral-1);
+        background-color: var(--color-primary-1);
+      `;
+    case '$edit':
+      return css`
+        color: var(--color-neutral-1);
+        background-color: var(--color-primary-1);
+      `;
+    case '$update':
+      return css`
+        color: var(--color-neutral-1);
+        background-color: var(--color-primary-1);
+      `;
+    case '$cancel':
+      return css`
+        color: var(--color-neutral-1);
+        background-color: var(--color-neutral-4);
+      `;
+    case '$delete':
+      return css`
+        color: var(--color-neutral-1);
+        background-color: var(--color-feedback-warning);
+      `;
+    case '$yes':
+      return css`
+        color: var(--color-neutral-1);
+        background-color: var(--color-feedback-success);
+      `;
+    case '$no':
+      return css`
+        color: var(--color-neutral-1);
+        background-color: var(--color-feedback-warning);
+      `;
+  }
+}; */
+
 const StyledButton = styled.button`
   font: var(--font-caption);
+  color: var(--color-neutral-1);
   text-transform: uppercase;
   display: flex;
   align-items: center;
   border-radius: var(--radius-m);
-  padding: 0.3rem 0.4rem;
+  padding: 0.3rem 0.6rem;
 
   svg {
-    padding-right: 1rem;
-    width: 40px;
-    height: 40 px;
+    fill: var(--color-neutral-1);
   }
 
   ${({ variant }) => {
     switch (variant) {
       case '$submit':
         return css`
-          color: var(--color-neutral-1);
           background-color: var(--color-primary-1);
           gap: var(--spacing-1);
           &:hover {
@@ -104,7 +149,6 @@ const StyledButton = styled.button`
         `;
       case '$edit':
         return css`
-          color: var(--color-neutral-1);
           background-color: var(--color-primary-1);
           gap: var(--spacing-1);
           &:hover {
@@ -113,7 +157,6 @@ const StyledButton = styled.button`
         `;
       case '$update':
         return css`
-          color: var(--color-neutral-1);
           background-color: var(--color-primary-1);
           gap: var(--spacing-1);
           &:hover {
@@ -122,27 +165,32 @@ const StyledButton = styled.button`
         `;
       case '$cancel':
         return css`
-          color: var(--color-neutral-1);
           background-color: var(--color-neutral-4);
           gap: var(--spacing-1);
+          &:hover {
+            background-color: var(--color-neutral-4-alpha25);
+          }
         `;
       case '$delete':
         return css`
-          color: var(--color-neutral-1);
           background-color: var(--color-feedback-warning);
           gap: var(--spacing-1);
+          &:hover {
+            background-color: var(--color-primary-2);
+          }
         `;
       case '$yes':
         return css`
-          color: var(--color-neutral-1);
           background-color: var(--color-feedback-success);
           gap: var(--spacing-1);
         `;
       case '$no':
         return css`
-          color: var(--color-neutral-1);
           background-color: var(--color-feedback-warning);
           gap: var(--spacing-1);
+          &:hover {
+            background-color: var(--color-primary-2);
+          }
         `;
     }
   }}
--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -6,13 +6,27 @@ export default function RecipesFilter() {
     <>
       <h1>Under construction...</h1>
       <Wrapper>
-        <Button variant='$submit'>Submit Recipe</Button>
-        <Button variant='$edit'>Edit</Button>
-        <Button variant='$update'>Update</Button>
-        <Button variant='$cancel'>Cancel</Button>
-        <Button variant='$delete'>Delete</Button>
-        <Button variant='$yes'>Yes</Button>
-        <Button variant='$no'>No</Button>
+        <Button variant='$submit' type={'button'}>
+          Submit Recipe
+        </Button>
+        <Button variant='$edit' type={'button'}>
+          Edit
+        </Button>
+        <Button variant='$update' type={'button'}>
+          Update
+        </Button>
+        <Button variant='$cancel' type={'button'}>
+          Cancel
+        </Button>
+        <Button variant='$delete' type={'button'}>
+          Delete
+        </Button>
+        <Button variant='$yes' type={'button'}>
+          Yes
+        </Button>
+        <Button variant='$no' type={'button'}>
+          No
+        </Button>
       </Wrapper>
     </>
   );
