2fafdb26dd9f58672577712bdab931bb9cd1dd50
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-15 09:21:40 +0200

    add accordion function to MainCategories

--- components/MainCategories.tsx
+++ components/MainCategories.tsx
@@ -1,20 +1,25 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import CategoryIcon from './CategoryIcon';
 import categories from '@/lib/categories.json';
+import { useState } from 'react';
 
 export default function MainCategories() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <StyledWrapper>
-      <StyledLinkGroup>
-        <StyledH2>Close Categories</StyledH2>
-        <StyledUl>
+      <StyledFilterGroup $isOpen={isOpen}>
+        <StyledToggleGroup onClick={() => setIsOpen(!isOpen)}>
+          <StyledH2>{isOpen ? 'Close Categories' : 'Open Categories'}</StyledH2>
+        </StyledToggleGroup>
+        <StyledUl $isOpen={isOpen}>
           {categories.map((category) => (
             <li key={category.id}>
               <CategoryIcon category={category} />
             </li>
           ))}
         </StyledUl>
-      </StyledLinkGroup>
+      </StyledFilterGroup>
     </StyledWrapper>
   );
 }
@@ -25,13 +30,27 @@ const StyledWrapper = styled.div`
   justify-content: center;
 `;
 
-const StyledLinkGroup = styled.div`
+const StyledFilterGroup = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   background-color: var(--color-primary-1);
   border-radius: var(--radius-m);
   width: 325px;
+  transition: all 0.5s ease-in-out;
+  overflow: hidden;
+
+  ${({ $isOpen }) =>
+    $isOpen &&
+    css`
+      max-height: 350px;
+    `}
+
+  ${({ $isOpen }) =>
+    !$isOpen &&
+    css`
+      max-height: 45px;
+    `}
 `;
 
 const StyledUl = styled.ul`
@@ -39,12 +58,34 @@ const StyledUl = styled.ul`
   gap: var(--spacing-2);
   grid-template-columns: repeat(5, auto);
   justify-content: space-between;
-  padding-block: var(--spacing-3);
   padding-inline: var(--spacing-3);
+  transition: all 0.5s ease-in-out;
+  overflow: hidden;
+  ${({ $isOpen }) =>
+    $isOpen &&
+    css`
+      max-height: 200px;
+      opacity: 1;
+      padding-block: var(--spacing-3);
+    `}
+  ${({ $isOpen }) =>
+    !$isOpen &&
+    css`
+      max-height: 40px;
+      opacity: 0;
+      padding-block: 0;
+    `}
 `;
+
+const StyledToggleGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+`;
+
 const StyledH2 = styled.h2`
   font: var(--font-headline-2);
   color: var(--color-neutral-1);
-  padding-block: var(--spacing-1);
+  padding-block: var(--spacing-2);
   align-self: center;
 `;
