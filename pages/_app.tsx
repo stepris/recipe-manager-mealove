c3352a6bbb6535d531abf36ef4752d10d2789a54
stepris <122841614+stepris@users.noreply.github.com>
2024-08-16 16:56:05 +0200

    Fix _app.tsx rebasing errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
     string[]
   >('favoriteRecipesList', { defaultValue: [] });
 
-  console.log('hi');
-
   const [recipes, setRecipes] = useLocalStorageState('recipes', {
     defaultValue: recipesJson,
   });
@@ -26,52 +24,53 @@ export default function App({ Component, pageProps }: AppProps) {
   const handleDeleteRecipe = (id: string) => {
     setRecipes(recipes.filter((recipe) => recipe.id !== id));
     router.push('/');
-
-    const handleEditRecipe = (recipeToUpdate: Recipe) => {
-      setRecipes((recipes) => {
-        return recipes.map((recipe) => {
-          if (recipe.id === recipeToUpdate.id) {
-            return recipeToUpdate;
-          } else {
-            return recipe;
-          }
-        });
-      });
-    };
-
-    const handleToggleFavorite: HandleToggleFavoriteFunction = (id) => {
-      const favoriteSet = new Set<string>(favoriteRecipesList);
-      if (!id) {
-        return;
-      } else {
-        if (favoriteSet.has(id)) {
-          favoriteSet.delete(id);
-        } else {
-          favoriteSet.add(id);
-        }
-        setFavoriteRecipesList(Array.from(favoriteSet));
-      }
-    };
-
-    const favoriteRecipes = recipes.filter((recipe) => {
-      return favoriteRecipesList.includes(recipe.id);
-    });
-    return (
-      <>
-        <GlobalStyle />
-        <Layout>
-          <Component
-            {...pageProps}
-            recipes={recipes}
-            onToggleFavorite={handleToggleFavorite}
-            favoriteRecipes={favoriteRecipes}
-            favoriteRecipesList={favoriteRecipesList}
-            onAddRecipe={handleAddRecipe}
-            onDeleteRecipe={handleDeleteRecipe}
-            onEditRecipe={handleEditRecipe}
-          />
-        </Layout>
-      </>
-    );
   };
+
+  const handleEditRecipe = (recipeToUpdate: Recipe) => {
+    setRecipes((recipes) => {
+      return recipes.map((recipe) => {
+        if (recipe.id === recipeToUpdate.id) {
+          return recipeToUpdate;
+        } else {
+          return recipe;
+        }
+      });
+    });
+  };
+
+  const handleToggleFavorite: HandleToggleFavoriteFunction = (id) => {
+    const favoriteSet = new Set<string>(favoriteRecipesList);
+    if (!id) {
+      return;
+    } else {
+      if (favoriteSet.has(id)) {
+        favoriteSet.delete(id);
+      } else {
+        favoriteSet.add(id);
+      }
+      setFavoriteRecipesList(Array.from(favoriteSet));
+    }
+  };
+
+  const favoriteRecipes = recipes.filter((recipe) => {
+    return favoriteRecipesList.includes(recipe.id);
+  });
+
+  return (
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Component
+          {...pageProps}
+          recipes={recipes}
+          onToggleFavorite={handleToggleFavorite}
+          favoriteRecipes={favoriteRecipes}
+          favoriteRecipesList={favoriteRecipesList}
+          onAddRecipe={handleAddRecipe}
+          onDeleteRecipe={handleDeleteRecipe}
+          onEditRecipe={handleEditRecipe}
+        />
+      </Layout>
+    </>
+  );
 }
