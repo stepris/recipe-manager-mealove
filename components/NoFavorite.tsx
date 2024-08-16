8a0d77f604d27635467dbeb0865365d496746f97
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-16 12:47:59 +0200

    add styling for noFavorite Component

--- components/NoFavorite.tsx
+++ components/NoFavorite.tsx
@@ -3,14 +3,35 @@ import styled from 'styled-components';
 export default function NoFavorite() {
   return (
     <StyledArticle>
-      <p>Sorry :(</p>
-      <p>There are no recipes in your favorite-list yet.</p>
-      <p>
-        You can add recipes, by clicking the heart 🥰 Icon in the right upper
-        corner of each recipe on the Explore-Page or on the Detail-Page.
-      </p>
+      <StyledParagraph>
+        <StyledSpan>
+          Sorry :( <br />
+          There are no recipes in your favorite-list yet.
+        </StyledSpan>
+      </StyledParagraph>
+      <StyledParagraph>
+        You can add recipes, by clicking the 🥰 in the right upper corner of
+        each recipe on the Explore-Page or on the Detail-Page.
+      </StyledParagraph>
     </StyledArticle>
   );
 }
 
-const StyledArticle = styled.article``;
+const StyledArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 80vh;
+  gap: var(--spacing-5);
+  text-align: center;
+`;
+
+const StyledParagraph = styled.p`
+  padding-inline: var(--spacing-5);
+  font: var(--font-base);
+`;
+
+const StyledSpan = styled.span`
+  font: var(--font-headline-2);
+  color: var(--color-primary-1);
+`;
