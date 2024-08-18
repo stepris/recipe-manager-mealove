4b39b558a9e993a90cbfb9740c3f3c56c26b1132
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-18 15:15:35 +0200

    add POST Route to api index.js

--- pages/api/recipes/index.js
+++ pages/api/recipes/index.js
@@ -7,6 +7,16 @@ export default async function handler(request, response) {
   if (request.method === 'GET') {
     const recipes = await Recipe.find();
     return response.status(200).json(recipes);
+  } else if (request.method === 'POST') {
+    try {
+      const recipeData = request.body;
+      await Recipe.create(recipeData);
+
+      response.status(201).json({ status: 'Recipe created' });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   } else {
     return response.status(405).json({ message: 'Method not allowed' });
   }
