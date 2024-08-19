b92fd367efb0f7906b0d4d85a6977c83612bd14c
Gunnar Montag <dev.gun.mon@gmail.com>
2024-08-19 12:54:43 +0200

    remove console.log

--- pages/api/recipes/index.js
+++ pages/api/recipes/index.js
@@ -14,7 +14,6 @@ export default async function handler(request, response) {
 
       response.status(201).json({ status: 'Recipe created' });
     } catch (error) {
-      console.log(error);
       response.status(400).json({ error: error.message });
     }
   } else {
