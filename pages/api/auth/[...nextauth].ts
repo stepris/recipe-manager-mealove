16aaf8865bc70a9b5e78db0a804435eafeb1a237
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-21 18:00:21 +0200

    fix naming in auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,13 +5,9 @@ import GoogleProvider from 'next-auth/providers/google';
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
@@ -28,5 +24,4 @@ export const authOptions = {
   //   },
   // },
 };
-
 export default NextAuth(authOptions);
