15578170da9632c1e2b86711300a4eb3d0e00385
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-21 18:12:40 +0200

    add google provider infos, google id to session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
@@ -11,17 +10,22 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  // callbacks: {
-  //   async jwt({ token, account, profile }) {
-  //     if (account && profile) {
-  //       token.userId = profile.id; // GitHub-User-ID zum JWT hinzufügen
-  //     }
-  //     return token;
-  //   },
-  //   async session({ session, token }) {
-  //     session.user.id = token.userId; // GitHub-User-ID zur Session hinzufügen
-  //     return session;
-  //   },
-  // },
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.provider = account.provider;
+        if (account.provider === 'google' && profile) {
+          token.userId = profile.sub; // Google User ID
+        }
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.userId;
+      session.user.provider = token.provider;
+      return session;
+    },
+  },
 };
+
 export default NextAuth(authOptions);
