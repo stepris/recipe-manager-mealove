cd7e6e4c646e306ae0424a9fea6f10a76b49360a
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-08-24 06:22:10 +0200

    secure PUT route

--- pages/api/recipes/[id].ts
+++ pages/api/recipes/[id].ts
@@ -1,6 +1,8 @@
 import dbConnect from '@/db/connect.ts';
 import Recipe from '@/db/models/Recipe';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(
   request: NextApiRequest,
@@ -8,6 +10,7 @@ export default async function handler(
 ) {
   await dbConnect();
 
+  const session = await getServerSession(request, response, authOptions);
   const { id } = request.query;
 
   if (request.method === 'GET') {
@@ -16,9 +19,32 @@ export default async function handler(
       return response.status(404).json({ status: 'Not Found' });
     }
     response.status(200).json(recipe);
-  } else if (request.method === 'PUT') {
+  }
+
+  if (request.method === 'PUT') {
+    // Check if Session
+    if (!session) {
+      return response.status(401).json({ message: 'Unauthorized' });
+    }
+    // Check if author id is session user id and if fetch recipe
     try {
       const recipeData = request.body;
+
+      if (!recipeData.authorId || !id) {
+        return response.status(400).json({ message: 'Invalid Request' });
+      }
+
+      const existingRecipe = await Recipe.findById(id);
+
+      if (!existingRecipe) {
+        return response.status(404).json({ message: 'Recipe not found' });
+      }
+
+      if (recipeData.authorId !== session.user?.id) {
+        return response.status(403).json({ message: 'Forbidden' });
+      }
+
+      // Update Recipe
       await Recipe.findByIdAndUpdate(id, recipeData);
       return response.status(200).json({ status: `Recipe ${id} updated!` });
     } catch (error: unknown) {
@@ -28,7 +54,9 @@ export default async function handler(
         response.status(400).json({ error: 'an unknown error occured' });
       }
     }
-  } else if (request.method === 'DELETE') {
+  }
+
+  if (request.method === 'DELETE') {
     try {
       await Recipe.findByIdAndDelete(id);
       return response
